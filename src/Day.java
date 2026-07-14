10e71e4049df973fa647b679da09aab04120ee0c
guru340 <gurusangwani06@gmail.com>
2026-07-14 13:38:46 +0530

    Ascci Implementation with String String and Prime Number

--- src/Day.java
+++ src/Day.java
@@ -2,38 +2,67 @@ import java.util.Scanner;
 
 public class Day {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int day=in.nextInt();
+//        Scanner in=new Scanner(System.in);
+//        int day=in.nextInt();
+//
+//        switch (day) {
+//            case 1:
+//            System.out.println("Monday");
+//                break;
+//            case 2:
+//            System.out.println("Tuesday");
+//            break;
+//            case 3:
+//            System.out.println("Wednesday");
+//            break;
+//            case 4:
+//            System.out.println("Thrusday");
+//            break;
+//            case 5:
+//            System.out.println("Friday");
+//            break;
+//            case 6:
+//            System.out.println("Saturday");
+//            break;
+//            case 7:
+//            System.out.println("Sunday");
+//
+//
+//
+//
+//
+//            default:
+//                System.out.println("You entered the wrong input");
+//                break;
+//        }
+        System.out.println(prime("noon"));
+    }
+    public static String prime(String str){
+        int sum=0;
+        String s="";
+        StringBuilder stringBuilder=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            int ascii=ch;
+            if(isPrime(ascii)){
+                sum+=ascii;
+                stringBuilder.append(ch);
+                s=String.valueOf(sum);
+            }
 
-        switch (day) {
-            case 1:
-            System.out.println("Monday");
-                break;
-            case 2:
-            System.out.println("Tuesday");
-            break;
-            case 3:
-            System.out.println("Wednesday");
-            break;
-            case 4:
-            System.out.println("Thrusday");
-            break;
-            case 5:
-            System.out.println("Friday");
-            break;
-            case 6:
-            System.out.println("Saturday");
-            break;
-            case 7:
-            System.out.println("Sunday");
-            
-            
-            
-            
-                
-            default:
-                System.out.println("You entered the wrong input");
-                break;
         }
+        if (stringBuilder.length() == 0) {
+            return str.charAt(0) + ":" + str.charAt(str.length() - 1);
+        }
+
+        return stringBuilder.toString()+":"+s;
+    }
+
+    public  static boolean isPrime(int n){
+        if (n<=1)return false;
+        for (int i=2;i*i<=n;i++){
+            if(n%i==0)return false;
+        }
+        return true;
     }
 }
