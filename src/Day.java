ad493077b767193477e800a910a8a930efa60079
guru340 <gurusangwani06@gmail.com>
2026-07-18 18:06:03 +0530

    POTD

--- src/Day.java
+++ src/Day.java
@@ -1,68 +1,77 @@
-import java.util.Scanner;
-
-public class Day {
-    public static void main(String[] args) {
-//        Scanner in=new Scanner(System.in);
-//        int day=in.nextInt();
+//public class Day {
+//    public static void main(String[] args) {
+////        Scanner in=new Scanner(System.in);
+////        int day=in.nextInt();
+////
+////        switch (day) {
+////            case 1:
+////            System.out.println("Monday");
+////                break;
+////            case 2:
+////            System.out.println("Tuesday");
+////            break;
+////            case 3:
+////            System.out.println("Wednesday");
+////            break;
+////            case 4:
+////            System.out.println("Thrusday");
+////            break;
+////            case 5:
+////            System.out.println("Friday");
+////            break;
+////            case 6:
+////            System.out.println("Saturday");
+////            break;
+////            case 7:
+////            System.out.println("Sunday");
+////
+////
+////
+////
+////
+////            default:
+////                System.out.println("You entered the wrong input");
+////                break;
+////        }
+//        System.out.println(reverseWords("Let's take LeetCode contest"));
 //
-//        switch (day) {
-//            case 1:
-//            System.out.println("Monday");
-//                break;
-//            case 2:
-//            System.out.println("Tuesday");
-//            break;
-//            case 3:
-//            System.out.println("Wednesday");
-//            break;
-//            case 4:
-//            System.out.println("Thrusday");
-//            break;
-//            case 5:
-//            System.out.println("Friday");
-//            break;
-//            case 6:
-//            System.out.println("Saturday");
-//            break;
-//            case 7:
-//            System.out.println("Sunday");
+//    }
+////    public static String prime(String str){
+////        int sum=0;
+////        String s="";
+////        StringBuilder stringBuilder=new StringBuilder();
+////        for(int i=0;i<str.length();i++){
+////            char ch=str.charAt(i);
+////            int ascii=ch;
+////            if(isPrime(ascii)){
+////                sum+=ascii;
+////                stringBuilder.append(ch);
+////                s=String.valueOf(sum);
+////            }
+////
+////        }
+////        if (stringBuilder.length() == 0) {
+////            return str.charAt(0) + ":" + str.charAt(str.length() - 1);
+////        }
+////
+////        return stringBuilder.toString()+":"+s;
+////    }
+////
+////    public  static boolean isPrime(int n){
+////        if (n<=1)return false;
+////        for (int i=2;i*i<=n;i++){
+////            if(n%i==0)return false;
+////        }
+////        return true;
+////    }
+//
+////    public static int  countHomogenous(String s) {
+////        int n=s.length();
+////        int i=0;
+////        while(i<n){
+////
+////        }
+////    }
 //
 //
-//
-//
-//
-//            default:
-//                System.out.println("You entered the wrong input");
-//                break;
-//        }
-        System.out.println(prime("noon"));
-    }
-    public static String prime(String str){
-        int sum=0;
-        String s="";
-        StringBuilder stringBuilder=new StringBuilder();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            int ascii=ch;
-            if(isPrime(ascii)){
-                sum+=ascii;
-                stringBuilder.append(ch);
-                s=String.valueOf(sum);
-            }
-
-        }
-        if (stringBuilder.length() == 0) {
-            return str.charAt(0) + ":" + str.charAt(str.length() - 1);
-        }
-
-        return stringBuilder.toString()+":"+s;
-    }
-
-    public  static boolean isPrime(int n){
-        if (n<=1)return false;
-        for (int i=2;i*i<=n;i++){
-            if(n%i==0)return false;
-        }
-        return true;
-    }
-}
+//}
