cea4726a7dfe2de33a407bd5aa8ce2552366a967
guru340 <gurusangwani06@gmail.com>
2026-09-23 10:55:55 +0530

    updated files

--- src/HCLTECH/secondlargest.java
+++ src/HCLTECH/secondlargest.java
@@ -5,7 +5,7 @@ public class secondlargest {
         int largest=Integer.MIN_VALUE;
         int secondlargest=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
+            if(arr[i]<largest){
                 secondlargest=largest;
                 largest=arr[i];
             } else if (arr[i]>secondlargest && secondlargest<largest) {
